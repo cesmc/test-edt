0359051276b37474f7fae41843ec404e499f2b06
cesmc <xccuevas@gmail.com>
2024-01-13 14:41:00 -0600

    feat: fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
+const URL = "https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  
+  const fetchData = () => {
+    axios.get(URL)
+    .then((response) => response.data)
+    .then((res) => {
+      setData(res);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  console.log('data', data)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TEST - EDT</h1>
+      
     </>
   )
 }
